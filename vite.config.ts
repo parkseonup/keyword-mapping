38d278ebdbc861ea7c87ddac4428f1aed73032d9
Seon u Park <seonupark@Seonui-MacBookPro.local>
2025-05-17 15:57:06 +0900

    chore: 절대 경로 import 설정

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: '@entites', replacement: '/src/entites' },
+      { find: '@shared', replacement: '/src/shared' },
+      { find: '@features', replacement: '/src/features' },
+      { find: '@', replacement: '/src' },
+    ],
+  },
 });
