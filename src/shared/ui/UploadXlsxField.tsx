9c198da2b182f900c9da9c82e0430c145a05b431
Seonu <seonupark@Seonui-MacBookPro.local>
2025-05-22 10:30:36 +0900

    feat: 엑셀 업로드시 에러 처리 추가

--- src/shared/ui/UploadXlsxField.tsx
+++ src/shared/ui/UploadXlsxField.tsx
@@ -4,6 +4,7 @@ import { DeleteOutlined, FileExcelOutlined } from '@ant-design/icons';
 import {
   Button,
   message,
+  Tooltip,
   Upload,
   type UploadFile,
   type UploadProps,
@@ -16,19 +17,15 @@ export interface Props {
 }
 
 const getRowData = async (file: File) => {
-  try {
-    const arrayBuffer = await file.arrayBuffer();
-    const parsed = read(arrayBuffer);
-    const workSheet = parsed.Sheets[parsed.SheetNames[0]];
-    return utils.sheet_to_json(workSheet, { header: 1 }); // header 1 지정시 배열로 변환
-  } catch (error) {
-    console.error(error);
-    message.error('파일 파싱에 실패했습니다.');
-  }
+  const arrayBuffer = await file.arrayBuffer();
+  const parsed = read(arrayBuffer);
+  const workSheet = parsed.Sheets[parsed.SheetNames[0]];
+  return utils.sheet_to_json(workSheet, { header: 1 }); // header 1 지정시 배열로 변환
 };
 
 export default function UploadXlsxField({ onChange, onRemove }: Props) {
   const [file, setFile] = useState<UploadFile | null>();
+  const [messageApi, contextHolder] = message.useMessage();
 
   const handleDeleteFile = () => {
     setFile(null);
@@ -44,36 +41,48 @@ export default function UploadXlsxField({ onChange, onRemove }: Props) {
         'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet';
 
       if (!isXlsx) {
-        message.error('xlsx 파일만 업로드 가능합니다!');
+        messageApi.error('xlsx 파일만 업로드 가능합니다!');
         return false;
       }
 
-      const data = await getRowData(file);
-      onChange(data);
-      setFile(file);
+      try {
+        const data = await getRowData(file);
+        onChange(data);
+        setFile(file);
+      } catch (error) {
+        console.error(error);
+        messageApi.error('파일 파싱에 실패했습니다.');
+      }
+
       return false; // 반환 값이 false일 시 > 서버 api 요청 안함
     },
     showUploadList: false,
   };
 
   return (
-    <div className="flex justify-end gap-2">
-      <Upload {...uploadProps}>
-        <Button
-          icon={<FileExcelOutlined />}
-          type="primary"
-          className="!rounded-button whitespace-nowrap bg-blue-600 hover:bg-blue-700"
-        >
-          Upload(.xlsx)
-        </Button>
-      </Upload>
-      {/** TODO: 삭제 버튼 구현 */}
-      <Button
-        icon={<DeleteOutlined />}
-        className="!rounded-button whitespace-nowrap"
-        onClick={handleDeleteFile}
-        disabled={!file}
-      ></Button>
-    </div>
+    <>
+      <div className="flex justify-end gap-2">
+        <Upload {...uploadProps}>
+          <Tooltip title="엑셀(.xlsx) 파일만 업로드">
+            <Button
+              icon={<FileExcelOutlined />}
+              type="primary"
+              className="!rounded-button whitespace-nowrap bg-blue-600 hover:bg-blue-700"
+            >
+              파일 업로드 (.xlsx)
+            </Button>
+          </Tooltip>
+        </Upload>
+        <Tooltip title="파일 삭제">
+          <Button
+            icon={<DeleteOutlined />}
+            className="!rounded-button whitespace-nowrap"
+            onClick={handleDeleteFile}
+            disabled={!file}
+          ></Button>
+        </Tooltip>
+      </div>
+      {contextHolder}
+    </>
   );
 }
